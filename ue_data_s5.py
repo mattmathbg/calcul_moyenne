1ea2417e223b5406d0cb03e49615679a98a1c705
Matteo_Mathis <mathis136u@etu.univ-lorraine.fr>
2025-11-27 12:26:37 +0100

    ajout

--- ue_data_s5.py
+++ ue_data_s5.py
@@ -81,7 +81,7 @@ ue_data_yiru = {
         "coef": 3,
         "grades": [(12, 0.5), (None, 0.5)]
     },
-     "AAS": {
+     "architecture": {
         "coef": 3,
         "grades": [(None, 0.5), (None, 0.5)]
     },
@@ -123,6 +123,49 @@ ue_data_ryad = {
      "Anglais": {
         "coef": 3,
         "grades": [(15, 0.5), (None, 0.5)]
+    },
+     "architecture": {
+        "coef": 3,
+        "grades": [(None, 0.5), (None, 0.5)]
+    },
+}
+
+ue_data_hugo = {
+    "LF": {
+        "coef": 3,
+        "grades": [(10, 0.5), (None, 0.5)]  # Notes de E1, E2 et E3
+    },
+    "CPOOA": {
+        "coef": 3,
+        "grades": [(None, 0.5), (None, 0.5)]
+    },
+    "Stats": {
+        "coef": 3,
+        "grades": [(11, 0.5), (None, 0.5)]
+    },
+    "CMBD": {
+        "coef": 3,
+        "grades": [(None, 0.5), (None, 0.5)]
+    },
+    "Application web": {
+        "coef": 3,
+        "grades": [(None, 0.5), (None, 0.25), (None, 0.25)]
+    },
+     "Systeme 2": {
+        "coef": 3,
+        "grades": [(7.5, 0.5), (None,0.5)]
+    },
+     "Réseaux 2": {
+        "coef": 3,
+        "grades": [(13.5, 0.5), (None, 0.5)]
+    },
+    "Analyse de données": {
+        "coef": 3,
+        "grades": [(None, 0.5), (None, 0.5)]
+    },
+     "Anglais": {
+        "coef": 3,
+        "grades": [(13.5, 0.5), (None, 0.5)]
     },
      "AAS": {
         "coef": 3,
